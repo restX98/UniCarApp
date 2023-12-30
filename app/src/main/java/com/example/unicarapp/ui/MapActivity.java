8930eab9c85f5413a6342b93fcb5ca632146a928
Enrico Restuccia <enrico.rest98@gmail.com>
2023-12-30 13:52:30 +0100

    Map Activity
    Added signout to map Activity

--- app/src/main/java/com/example/unicarapp/ui/MapActivity.java
+++ app/src/main/java/com/example/unicarapp/ui/MapActivity.java
@@ -3,20 +3,34 @@ package com.example.unicarapp.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.unicarapp.R;
+import com.example.unicarapp.data.model.User;
 import com.example.unicarapp.databinding.ActivityMapBinding;
 import com.example.unicarapp.viewmodels.MapViewModel;
 import com.google.android.material.navigation.NavigationView;
 
 public class MapActivity extends AppCompatActivity {
+
+    MapViewModel mapViewModel;
     private ActivityMapBinding binding;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!mapViewModel.isAuthenticated()) {
+            startActivity(new Intent(this, MapActivity.class));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +38,35 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
 
         ActivityMapBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_map);
-        MapViewModel viewModel = new ViewModelProvider(this).get(MapViewModel.class);
-        binding.setViewModel(viewModel);
+
+        mapViewModel = new ViewModelProvider(this).get(MapViewModel.class);
 
         drawerLayout = binding.drawerLayout;
         navigationView = binding.navView;
 
+        mapViewModel.getCurrentUserLiveData().observe(this, new CurrentUserObserver());
+
         binding.fabProfile.setOnClickListener(new ClickHandler());
+        binding.fabSignout.setOnClickListener(new ClickHandler());
+    }
+
+    private class CurrentUserObserver implements Observer<User> {
+        @Override
+        public void onChanged(User user) {
+            if(user == null) {
+                startActivity(new Intent(MapActivity.this, LoginActivity.class));
+            }
+        }
     }
 
     private class ClickHandler implements View.OnClickListener {
-
         @Override
         public void onClick(View v) {
             int btnId = v.getId();
             if(btnId == R.id.fab_profile) {
-                binding.drawerLayout.openDrawer(binding.navView);
+                drawerLayout.openDrawer(navigationView);
+            } else if( btnId == R.id.fab_signout) {
+                 mapViewModel.signOut();
             }
         }
     }
--- app/src/main/java/com/example/unicarapp/viewmodels/MapViewModel.java
+++ app/src/main/java/com/example/unicarapp/viewmodels/MapViewModel.java
@@ -1,8 +1,29 @@
 package com.example.unicarapp.viewmodels;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.unicarapp.data.model.User;
+import com.example.unicarapp.data.repository.UserRepository;
 
 public class MapViewModel extends ViewModel {
 
-    public MapViewModel() {}
+    private UserRepository userRepository;
+
+    public MapViewModel() {
+        this.userRepository = new UserRepository();
+    }
+
+    public boolean isAuthenticated() {
+        return userRepository.isAuthenticated();
+    }
+
+    public void signOut() {
+        userRepository.signOut();
+    }
+
+    public MutableLiveData<User> getCurrentUserLiveData() {
+        return userRepository.getCurrentUserLiveData();
+    }
+
 }
